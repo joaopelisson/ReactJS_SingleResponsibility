da4a8d5043f61f9a8febface16ce9937f366a2b7
João <joaopedro.devbr@gmail.com>
2024-08-16 14:55:43 -0300

    feat: applying Single Responsibility Principle

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,34 @@ import axios from "axios";
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function App() {
-  const [data, setData] = useState([]);
+enum EGENDER {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
+
+interface IPeople {
+  name: {
+    first: string;
+    last: string;
+  };
+  cell: string;
+  gender: EGENDER;
+}
+
+interface IReturnPeople {
+  results: IPeople[];
+}
+
+const useApp = () => {
+  const [data, setData] = useState<Array<IPeople>>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleFetch = async () => {
     setIsLoading(true);
-    const response = await axios.get(`https://randomuser.me/api/?results=20`);
-    setData(response.data);
+    const response = await axios.get<IReturnPeople>(
+      `https://randomuser.me/api/?results=20`
+    );
+    setData(response.data.results);
     setIsLoading(false);
   };
 
@@ -17,19 +37,31 @@ function App() {
     handleFetch();
   }, []);
 
+  return {
+    data,
+    isLoading,
+  };
+};
+
+const getColorByGender = (gender: EGENDER) => {
+  return gender.toUpperCase() === EGENDER.MALE ? "blue" : "red";
+};
+
+function App() {
+  const { data, isLoading } = useApp();
 
   return (
     <main>
       <h1>Single responsibility principle</h1>
       {isLoading && <strong>Loading...</strong>}
-      {data?.results?.map((item) => {
+      {data?.map((item) => {
         return (
           <div key={item.cell}>
-              <p
-                style={{color: item.gender === 'male' ? 'blue' : 'red'}}
-              >{item.name.first} {item.name.last}</p>
+            <p style={{ color: getColorByGender(item.gender) }}>
+              {item.name.first} {item.name.last}
+            </p>
           </div>
-        )
+        );
       })}
     </main>
   );
